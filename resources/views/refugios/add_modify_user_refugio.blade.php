8ece0c6871d47bf2408dccbde4851e97b43b60bd
chavesandres1009 <andresch1009@gmail.com>
2016-10-30 00:36:05 -0600

    Cambios en mascotas, y en el registro de refugios

---
+++ resources/views/refugios/add_modify_user_refugio.blade.php
@@ -0,0 +1,145 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" @if(isset($refugio)) action="{{ url('/' . $refugio->id . '/guardar_refugio_user') }}" @else action="{{ url('/guardar_refugio_user') }}" @endif enctype="multipart/form-data">
+                        {{ csrf_field() }}
+
+                        @if(isset($refugio))
+                        <img style="heigth: 100px; width: 100px;" src="../storage/refugios/{{ $refugio->logo }}" class="img-responsive"  alt="">
+                        @endif
+
+                        <div class="form-group{{ $errors->has('nombre') ? ' has-error' : '' }}">
+                            <label for="nombre" class="col-md-4 control-label">Nombre</label>
+
+                            <div class="col-md-6">
+                                <input id="nombre" type="text" class="form-control" name="nombre" @if (isset($refugio)) value="{{ $refugio->nombre }}" @endif>
+
+                                @if ($errors->has('nombre'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('nombre') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('direccion') ? ' has-error' : '' }}">
+                            <label for="direccion" class="col-md-4 control-label">Dirección</label>
+
+                            <div class="col-md-6">
+                                <textarea type="text" class="form-control" name="direccion">@if (isset($refugio)) {{ $refugio->direccion }} @endif </textarea>
+
+                                @if ($errors->has('direccion'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('direccion') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('descripcion') ? ' has-error' : '' }}">
+                            <label for="descripcion" class="col-md-4 control-label">Descripción</label>
+
+                            <div class="col-md-6">
+                                <textarea type="email" class="form-control" name="descripcion">@if (isset($refugio)) {{ $refugio->descripcion }} @endif </textarea>
+
+                                @if ($errors->has('descripcion'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('direccion') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('telefono') ? ' has-error' : '' }}">
+                            <label for="telefono" class="col-md-4 control-label">Teléfono</label>
+
+                            <div class="col-md-6">
+                                <input type="tel" class="form-control" name="telefono" @if (isset($refugio)) value="{{ $refugio->telefono }}" @endif>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('telefono') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">Correo</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" @if (isset($refugio)) value="{{ $refugio->email }}" @endif required >
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('logo') ? ' has-error' : '' }}">
+                            <label for="logo" class="col-md-4 control-label">Logo</label>
+
+                            <div class="col-md-6">
+                                <input id="logo" type="file" class="form-control" name="logo">
+
+                                @if ($errors->has('logo'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('logo') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Contraseña</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirmar Contraseña</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                  @if(isset($refugio))
+                                    Modificar Refugio
+                                  @else
+                                    Registrar Refugio
+                                  @endif
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
