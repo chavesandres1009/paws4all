77593401470300d71be3ae8f0acd38260939aae2
arepaconmiel <jvillalobosvega@hotmail.com>
2016-11-02 20:57:23 -0600

    Create exito.blade.php

---
+++ resources/views/paypal/exito.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div>
+      Su transaccion se a realizado con exito. Gracias por su ayuda.
+      </div>
+    </div>
+  </div>
+@endsection
