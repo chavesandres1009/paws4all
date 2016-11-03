39eb01b91f1e82dd6818793c6218a5a306ea5ff6
arepaconmiel <jvillalobosvega@hotmail.com>
2016-11-02 20:57:04 -0600

    Create error.blade.php

---
+++ resources/views/paypal/error.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div>
+      Hubo un error con la trasaccion.Por favor vuelvalo a intentar. De seguir teniendo problemas contacte con nosotros.
+    </div>
+  </div>
+  </div>
+@endsection
